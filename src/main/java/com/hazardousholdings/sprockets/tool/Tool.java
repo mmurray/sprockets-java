ed78a85cd44f0bebda6419c27453655bb1384137
Michael Murray <mmurray.wa@gmail.com>
2012-07-11 16:55:43 -0700

    tool so far

---
+++ src/main/java/com/hazardousholdings/sprockets/tool/Tool.java
@@ -0,0 +1,13 @@
+package com.hazardousholdings.sprockets.tool;
+
+public class Tool {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("woooo!");
+
+	}
+
+}
